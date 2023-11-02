f42c0da1a3b9742bb064dac7ba17e4cba4b4fdd0
hritik2002 <sharmahritik2002@gmail.com>
2023-11-03 00:08:40 +0530

    feat: add api endpoint to customize meal

--- utils/promptsGenerator.js
+++ utils/promptsGenerator.js
@@ -51,6 +51,39 @@ const mealPlannerPromptGenerator = (userDetail) => {
         `
 }
 
+const customizeMealPromptGenerator = (userDetail, prevMeal) => {
+    return `
+    Design a balanced meal plan that includes a distinct recipe for the given meal. 
+    Ensure that recipe aligns with the user's macro goals, including calories, carbohydrates, protein, and fats. 
+    Please provide the recipe name and a list of ingredients for each meal. 
+    Consider the user's dietary preferences and the previous meal and aim to create a new meal that are nutritionally balanced and suitable for their health goals and the count of calorie, carbohydrates, protein, and fats should be similar to previous meal.
+
+    This is the user details:
+
+    ${userDetail}
+
+    This is previous meal:
+
+    ${prevMeal}
+
+    Only return a json response in the following schema:
+
+    {
+        recipe_name,
+        ingredients,
+        macro_goals:{
+            calories,
+            carbs,
+            protein,
+            fats
+        },
+    }
+
+    Do not return anything except the json response:
+    `
+}
+
 module.exports = {
-    mealPlannerPromptGenerator
+    mealPlannerPromptGenerator,
+    customizeMealPromptGenerator
 }
